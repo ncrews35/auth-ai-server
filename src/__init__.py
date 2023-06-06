b7438037a06bfc83cde12660b7a57d4171e8da00
Nick Crews <nickcrews35@gmail.com>
2023-06-05 23:29:28 -0400

    Remove api code

--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,5 @@ from .utils import *
 __all__ = [
     "ErrorCode",
     "error",
-    "authenticate_user_credentials",
-    "authenticate_client",
-    "generate_access_token",
-    "generate_authorization_code",
-    "verify_authorization_code",
-    "verify_client_info",
+    "status_code"
 ]
